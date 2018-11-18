357d26c899d0f746bc7dd08ad9f765f7f997957b
Angelica Abbate <angelicabbate@gmail.com>
2018-11-18 15:09:44 -0300

    Add responsive adjustments, portfolio images, and mobile menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 window.onload = function() {
+    // ----------- MENU MOBILE BTN SETUP
+    document.getElementById('mobile-menu-btn').addEventListener('click', function(e) {
+        if(document.getElementById('menu-mobile').classList.contains('active')) {
+            document.getElementById('menu-mobile').classList.remove('active');
+        } else {
+            document.getElementById('menu-mobile').classList.add('active');
+        }
+    });
+
+    // -------- SMOOTH SCROLLING SETUP ---------
+    document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+        if (anchor.getAttribute('href') === '#') {
+            return;
+        }
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        });
+    });
 
     // ---------- PORTFOLIO SWIPER
     var swiperPortfolio = new Swiper('.swiper-container', {
@@ -15,7 +38,8 @@ window.onload = function() {
         loop : true,
         autoplay: {
             delay: 8000,
-        }
+        },
+        autoHeight: true,
       });
 
 }
